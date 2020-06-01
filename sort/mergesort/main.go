0f46372016d03be938f0d49e212c9978cc9c355c
dr.yang <771596885@qq.com>
2020-06-01 23:23:34 +0800

    归并排序提交

--- sort/mergesort/main.go
+++ sort/mergesort/main.go
@@ -2,51 +2,56 @@ package main
 
 import "fmt"
 
+var assit []int
+
 func main() {
 	arr := []int{2, 4, 10, 8, 1, 4, 3, 5, 7}
-	fmt.Println(merge_sort(arr, 0, len(arr)-1))
+	assit = make([]int, len(arr))
+	merge_sort(arr, 0, len(arr)-1)
+	fmt.Println(arr)
 }
 
-func merge_sort(arr []int, start, end int) []int {
-	mid := (start + end) / 2
-	arr1 := merge_sort(arr, start, mid)
-	arr2 := merge_sort(arr, mid+1, end)
-	return merge(arr1, arr2)
+func merge_sort(arr []int, left, right int) {
+	if left >= right {
+		return
+	}
+	mid := (left + right) / 2
+	merge_sort(arr, left, mid)
+	merge_sort(arr, mid+1, right)
+	merge(arr, left, mid, right)
 }
 
-func merge(arr1, arr2 []int) []int {
-	var temp []int
-	var (
-		i = 0
-		j = 0
-	)
-	for {
-		if arr1[i] < arr2[j] {
-			temp = append(temp, arr1[i])
+func merge(arr []int, left, mid, right int) {
+	i := left
+	p1 := left
+	p2 := mid + 1
+
+	for p1 <= mid && p2 <= right {
+		// 两个归并的数组都没有越界，则开始合并
+		if arr[p1] < arr[p2] {
+			assit[i] = arr[p1]
+			p1++
 			i++
 		} else {
-			temp = append(temp, arr1[j])
-			j++
-		}
-		if i >= len(arr1) || j >= len(arr2) {
-			break
+			assit[i] = arr[p2]
+			p2++
+			i++
 		}
 	}
-	//放左边的剩余元素
-	for {
-		temp = append(temp, arr1[i])
+	// 如果是左子组的元素先放完
+	for p2 <= right {
+		assit[i] = arr[p2]
 		i++
-		if i >= len(arr1) {
-			break
-		}
+		p2++
 	}
-	//放右边的剩余元素
-	for {
-		temp = append(temp, arr1[j])
-		j++
-		if j >= len(arr2) {
-			break
-		}
+	// 如果是右子组先放完
+	for p1 <= mid {
+		assit[i] = arr[p1]
+		i++
+		p1++
 	}
-	return temp
+	// 将assit复制到原数组中
+	for index := left; index <= right; index++ {
+		arr[index] = assit[index]
+	}
 }
